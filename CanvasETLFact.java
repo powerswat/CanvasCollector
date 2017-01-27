c9e967549051bc3facb7e67f35154b119a012c70
Young Suk Cho <youngsukcho@Kuis-MacBook-Air.local>
2017-01-27 10:56:33 -0500

    Applying Factory approach

---
+++ CanvasETLFact.java
@@ -0,0 +1,8 @@
+/**
+ * Created by youngsukcho on 2017. 1. 26..
+ */
+public interface CanvasETLFact {
+    public void readAPI(String addr);
+    public void tabulate();
+    public void insertToDB();
+}
---
+++ CourseETL.java
@@ -0,0 +1,22 @@
+/**
+ * Created by youngsukcho on 2017. 1. 26..
+ */
+public class CourseETL implements CanvasETLFact {
+
+    private String jsonStr = "";
+
+    @Override
+    public void readAPI(String addr) {
+
+    }
+
+    @Override
+    public void tabulate() {
+
+    }
+
+    @Override
+    public void insertToDB() {
+
+    }
+}
