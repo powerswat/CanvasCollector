a8d0eb9cd57f393a97015d883754b91a627b0bfc
Young Suk Cho <cho.648@osu.edu>
2017-01-27 12:51:22 -0500

    Design Factory,Singleton Structure and set up configuration

--- CollectorDriver.java
+++ CollectorDriver.java
@@ -1,8 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by youngsukcho on 2017. 1. 24..
  */
 public class CollectorDriver {
-    public static void main(String[] args){
 
+    private static String webAddr;
+    private static String token;
+    private static String apiPath = "api/v1/";
+
+    // Read the config file and parse it
+    private static void loadConfig(String configPath){
+        Properties pFile = new Properties();
+        try {
+            InputStream input = new FileInputStream(configPath);
+            pFile.load(input);
+            webAddr = pFile.getProperty("webAddr");
+            token = pFile.getProperty("token");
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Entry point to the system
+    public static void main(String[] args){
+        // Read the config file and parse it
+        loadConfig("./config/apiconfig.properties");
+
+        // Start collecting course information from the given token
+        CourseETL cEtl = CourseETL.getInstance();
+        cEtl.runProcess();
     }
 }
--- CourseETL.java
+++ CourseETL.java
@@ -2,8 +2,23 @@
  * Created by youngsukcho on 2017. 1. 26..
  */
 public class CourseETL implements CanvasETLFact {
-
+    private static CourseETL instance = null;
     private String jsonStr = "";
+    private String coursePath = "courses";
+
+    public static CourseETL getInstance() {
+        if (instance == null)
+            synchronized (CourseETL.class) {
+                if (instance == null)
+                    instance = new CourseETL();
+            }
+        return instance;
+    }
+
+    // Start collecting course information from the given token
+    public void runProcess(){
+
+    }
 
     @Override
     public void readAPI(String addr) {
